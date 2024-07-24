84e807ea80f0c796a77cf0f2b1da8626c3321c1e
Cristhofer24 <cristhofer.elian.gramal@gmail.com>
2024-07-23 23:08:40 -0500

    Desarrollo de inicio del nuevo persentil

---
+++ percentil.js
@@ -0,0 +1,64 @@
+const ctx = document.getElementById('growthChart').getContext('2d');
+const growthChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['0', '1', '2', '3', '4', '5'],
+        datasets: [
+            {
+                label: '97th Percentile',
+                data: [50],
+                borderColor: 'rgba(255, 159, 64, 1)',
+                fill: false
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Edad (años y meses)'
+                },
+                ticks: {
+                    autoSkip: false, // Evita que se omitan las etiquetas
+                    maxRotation: 0, // Evita la rotación de etiquetas
+                    callback: function (value) {
+                        return value; // Mostrar las etiquetas tal como están definidas
+                    }
+                },
+                stepSize: 1,
+                min: 0,
+                max: 5
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Longitud/Estatura (cm)'
+                },
+                ticks: {
+                    stepSize: 5,
+                    callback: function (value) {
+                        return Number.isInteger(value) ? value : '';
+                    }
+                },
+                min: 45,
+                max: 125
+            },
+            y1: {
+                position: 'right',
+                title: {
+                    display: true
+                },
+                ticks: {
+                    stepSize: 5,
+                    callback: function (value) {
+                        return Number.isInteger(value) ? value : '';
+                    }
+                },
+                min: 45,
+                max: 125
+            },
+        }
+    }
+});
