4c8269d29833ce3a464382a49cddc98b577e1010
Cristhofer24 <cristhofer.elian.gramal@gmail.com>
2024-07-27 21:36:26 -0500

    Revision de puntos

--- percentil.js
+++ percentil.js
@@ -13,17 +13,17 @@ const growthChart = new Chart(ctx, {
                 borderColor: 'black',
                 borderWidth: 2,
                 data: [
-                    { x:0, y: 55 },
-                    { x: 1, y: 77 },
-                    { x: 2, y: 90 },
-                    { x: 3, y: 103 },
-                    { x: 4, y: 114 },
-                    { x: 5, y: 125 },
+                    { x:0, y: 56 },
+                    { x: 1, y: 83 },
+                    { x: 2, y: 97 },
+                    { x: 3, y: 107.5 },
+                    { x: 4, y: 116 },
+                    { x: 5, y: 124},
 
                 ],
                 tension: 0.2,
                 fill: false,
-                pointRadius:0
+                pointRadius:2
             },
 
             {   label:'red',
@@ -31,16 +31,16 @@ const growthChart = new Chart(ctx, {
                 borderWidth: 2,
                 data: [
                     { x:0, y: 54 },
-                    { x: 1, y: 73 },
-                    { x: 2, y: 86},
-                    { x: 3, y: 99 },
-                    { x: 4, y: 110 },
-                    { x: 5, y: 120},
+                    { x: 1, y: 80.5 },
+                    { x: 2, y: 94},
+                    { x: 3, y: 103.5 },
+                    { x: 4, y: 112 },
+                    { x: 5, y: 119.5},
 
                 ],
                 tension: 0.2,
                 fill: false,
-                pointRadius:0
+                pointRadius:2
             },
             
             {   label:'green',
@@ -48,48 +48,48 @@ const growthChart = new Chart(ctx, {
                 borderWidth: 2,
                 data: [
                     { x:0, y: 50 },
-                    { x: 1, y: 68 },
-                    { x: 2, y: 80},
-                    { x: 3, y: 90 },
-                    { x: 4, y: 100 },
+                    { x: 1, y: 76},
+                    { x: 2, y: 87.5},
+                    { x: 3, y: 96},
+                    { x: 4, y: 103.5 },
                     { x: 5, y: 110},
 
                 ],
                 tension: 0.2,
                 fill: false,
-                pointRadius:0
+                pointRadius:2
             },
             {   label:'red',
                 borderColor: 'red',
                 borderWidth: 2,
                 data: [
                     { x:0, y: 46 },
-                    { x: 1, y: 62 },
-                    { x: 2, y: 71},
-                    { x: 3, y: 80 },
-                    { x: 4, y: 90 },
-                    { x: 5, y: 100},
+                    { x: 1, y: 71 },
+                    { x: 2, y: 81.5},
+                    { x: 3, y: 88.5 },
+                    { x: 4, y: 95 },
+                    { x: 5, y: 101},
 
                 ],
                 tension: 0.2,
                 fill: false,
-                pointRadius:0
+                pointRadius:2
             },
             {   label:'Black',
                 borderColor: 'black',
                 borderWidth: 2,
                 data: [
                     { x:0, y: 44 },
-                    { x: 1, y: 58 },
-                    { x: 2, y: 67 },
-                    { x: 3, y: 76},
-                    { x: 4, y: 86},
+                    { x: 1, y: 68.5},
+                    { x: 2, y: 78.5 },
+                    { x: 3, y: 85},
+                    { x: 4, y: 91},
                     { x: 5, y: 96 },
 
                 ],
                 tension: 0.2,
                 fill: false,
-                pointRadius:0
+                pointRadius:2
             },
                 // Dataset para puntos calculados
                 {
